b4c8bc39b2c0168e22e0d18b417347cbaa0b195b
GrufasXd <147091694+GrufasXd@users.noreply.github.com>
2024-03-18 21:09:01 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -11,6 +11,7 @@ struct studentas {
     std::vector<int> nd;
     int egzas;
 };
+void skaitytifaila(int rakt, std::vector<studentas>& vargsai, std::vector<studentas>& galva, std::vector<double> galutrez1, std::vector<double> galutrez2, std::vector<double> galutrezg, std::vector<double> median1, std::vector<double> median2, std::vector<double> mediang);
 void studrus(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median, std::vector<studentas>& vargsai, std::vector<studentas>& galva, const std::string& filename);
 void createfile(const std::string& filename, const int& kiekis);
 void skaitymas(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median);
