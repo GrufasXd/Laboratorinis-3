b6309a6ae1c9ccdcb257f11de11e1d53799b1daf
GrufasXd <147091694+GrufasXd@users.noreply.github.com>
2024-03-26 23:30:11 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -4,14 +4,16 @@
 #include <string>
 #include <vector>
 #include <functional>
-
+using namespace std;
 struct studentas {
     std::string vardas;
     std::string pavarde;
     std::vector<int> nd;
     int egzas;
 };
-void skaitytifaila(int rakt, std::vector<studentas>& vargsai, std::vector<studentas>& galva, std::vector<double> galutrez1, std::vector<double> galutrez2, std::vector<double> galutrezg, std::vector<double> median1, std::vector<double> median2, std::vector<double> mediang);
+int partition(vector<studentas>& students, vector<double>& galrez, vector<double>& median, int low, int high, int rusis);
+void quickSort(vector<studentas>& students, vector<double>& galrez, vector<double>& median, int low, int high, int rusis);
+void processChunk(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median, std::vector<studentas>& vargsai, std::vector<studentas>& galva, const std::vector<std::string>& chunkData, int rakt);
 void studrus(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median, std::vector<studentas>& vargsai, std::vector<studentas>& galva, const std::string& filename);
 void createfile(const std::string& filename, const int& kiekis);
 void skaitymas(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median);
