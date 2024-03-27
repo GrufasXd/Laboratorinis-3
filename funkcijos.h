a34132ce436f5c04161804e9670a8f4698b1e2fa
GrufasXd <147091694+GrufasXd@users.noreply.github.com>
2024-03-27 13:13:10 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -11,10 +11,9 @@ struct studentas {
     std::vector<int> nd;
     int egzas;
 };
-void sortAndWriteToFile(vector<studentas>& students, vector<double>& galrez, vector<double>& median, const int& ilgis, const string& filename);
 int partition(vector<studentas>& students, vector<double>& galrez, vector<double>& median, int low, int high, int rusis);
 void quickSort(vector<studentas>& students, vector<double>& galrez, vector<double>& median, int low, int high, int rusis);
-void studrus(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median, std::vector<studentas>& vargsai, std::vector<studentas>& galva, const std::string& filename);
+void studrus(vector<studentas>& students, vector<double>& galrez, vector<double>& median, vector<studentas>& vargsai, vector<studentas>& galva, const string& filename);
 void createfile(const std::string& filename, const int& kiekis);
 void skaitymas(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median);
 void rasytiranka(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median);
