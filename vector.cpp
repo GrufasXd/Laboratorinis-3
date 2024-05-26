8b04ae58ffcf58938787e9ee8be4d967a5a7ca02
GrufasXd <147091694+GrufasXd@users.noreply.github.com>
2024-05-26 19:52:19 +0100

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -16,31 +16,71 @@ int main() {
     vector<double> median;
     int kodas;
     int rusis;
+    int par;
     auto start = high_resolution_clock::now();
     createfile("failas1.txt", 1000);
     auto end = high_resolution_clock::now();
     duration<double> diff = end-start;
     cout << "Sukurti 1000 elementu faila uztruko " << diff.count() << " s" << endl;
+    cout << "Kokia studentu rusiavimo strategija naudot: " << endl;
+    cout << "1 strategija - spausti 1 " << endl;
+    cout << "2 strategija - spausti 2 " << endl;
+    cout << "3 strategija - spausti 3 " << endl;
+    cin >> par;
+    if(par == 1)
+    studrus(students, vargsai, galva, "failas1.txt",1000);
+    else if(par == 2)
+    studrus1(students, vargsai, "failas1.txt",1000);
+    else if(par == 3)
     studrus2(students, vargsai, galva, "failas1.txt",1000);
     auto start1 = high_resolution_clock::now();
     createfile("failas2.txt", 10000);
     auto end1 = high_resolution_clock::now();
     duration<double> diff1 = end1-start1;
     cout << "Sukurti 10000 elementu faila uztruko " << diff1.count() << " s" << endl;
+    cout << "Kokia studentu rusiavimo strategija naudot: " << endl;
+    cout << "1 strategija - spausti 1 " << endl;
+    cout << "2 strategija - spausti 2 " << endl;
+    cout << "3 strategija - spausti 3 " << endl;
+    cin >> par;
+    if(par == 1)
+    studrus(students, vargsai, galva, "failas2.txt",10000);
+    else if(par == 2)
+    studrus1(students, vargsai, "failas2.txt",10000);
+    else if(par == 3)
     studrus2(students, vargsai, galva, "failas2.txt",10000);
     auto start2 = high_resolution_clock::now();
     createfile("failas3.txt", 100000);
     auto end2 = high_resolution_clock::now();
     duration<double> diff2 = end2-start2;
     cout << "Sukurti 100000 elementu faila uztruko " << diff2.count() << " s" << endl;
+    cout << "Kokia studentu rusiavimo strategija naudot: " << endl;
+    cout << "1 strategija - spausti 1 " << endl;
+    cout << "2 strategija - spausti 2 " << endl;
+    cout << "3 strategija - spausti 3 " << endl;
+    cin >> par;
+    if(par == 1)
+    studrus(students, vargsai, galva, "failas3.txt",100000);
+    else if(par == 2)
+    studrus1(students, vargsai, "failas3.txt",100000);
+    else if(par == 3)
     studrus2(students, vargsai, galva, "failas3.txt",100000);
     auto start3 = high_resolution_clock::now();
     createfile("failas4.txt", 1000000);
     auto end3 = high_resolution_clock::now();
     duration<double> diff3 = end3-start3;
     cout << "Sukurti 1000000 elementu faila uztruko " << diff3.count() << " s" << endl;
-    studrus2(students, vargsai, galva, "failas4.txt", 1000000);
-    auto start4 = high_resolution_clock::now();
+    cout << "Kokia studentu rusiavimo strategija naudot: " << endl;
+    cout << "1 strategija - spausti 1 " << endl;
+    cout << "2 strategija - spausti 2 " << endl;
+    cout << "3 strategija - spausti 3 " << endl;
+    cin >> par;
+    if(par == 1)
+    studrus(students, vargsai, galva, "failas4.txt",1000000);
+    else if(par == 2)
+    studrus1(students, vargsai, "failas4.txt",1000000);
+    else if(par == 3)
+    studrus2(students, vargsai, galva, "failas4.txt",1000000);
      bool correctInput = false;
 
     /*while (!correctInput) {
